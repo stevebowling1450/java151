35f48c128d009c16e5d06758931c850d93848247
Steve <stevebowling@SteveBowlingMBP.local>
2016-10-03 11:52:06 -0400

    var

---
+++ src/_001_helloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+package _001_helloWorld;
+
+/**
+ * Created by stevebowling on 10/3/16.
+ * This class prints out Hello World
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+
+
+        System.out.println("Knock Knock");
+        System.out.println("Who is there?");
+        System.out.println("boo");
+        System.out.println("Boo Who?");
+        System.out.println("Don't Cry");
+        System.out.println("Hello World");
+        System.out.println("Hello, World!");
+        System.out.println("Hello Class");
+        System.out.println("Hello, Nurse!");
+
+    }
+}
+
+
---
+++ src/_002_Variables/BasicConcepts.java
@@ -0,0 +1,25 @@
+package _002_Variables;
+
+/**
+ * Created by stevebowling on 10/3/16.
+ *  This we are going over basic concepts on variables
+ */
+
+public class BasicConcepts {
+
+
+
+    public static void main(String args[]){
+
+        String userName = "Steve";
+    int newNumber = 4;
+    boolean tobeornottobe = true;
+    System.out.println(userName);
+        System.out.println(newNumber);
+        System.out.println(tobeornottobe);
+
+        
+
+
+}
+}
